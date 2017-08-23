f93dddaf1f0f819c3213dfafe4aa0a3175aaaf84
Víctor Déniz Falcón <fitodeniz@gmail.com>
2017-08-23 16:54:31 +0100

    Initial project

---
+++ routes/web.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It is a breeze. Simply tell Lumen the URIs it should respond to
+| and give it the Closure to call when that URI is requested.
+|
+*/
+
+$app->get('/', function () use ($app) {
+    return $app->version();
+});
+
+$app->get('/hello/world', function() use ($app) {
+    return "Hello world!";
+});
+
+$app->get('/hello/{name}', function ($name) use ($app) {
+    return "Hello {$name}";
+});
+
